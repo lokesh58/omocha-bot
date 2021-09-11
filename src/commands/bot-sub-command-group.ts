e208f66c022a1a56008f117deead1ea4e56c9d28
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-09-11 22:21:43 +0530

    Added interface for sub command groups

---
+++ src/commands/bot-sub-command-group.ts
@@ -0,0 +1,9 @@
+import { ApplicationCommandSubGroupData, Client, CommandInteraction } from 'discord.js';
+
+interface BotSubCommandGroup {
+  data: ApplicationCommandSubGroupData,
+  once?: (client: Client) => void,
+  handler: (interaction: CommandInteraction) => void,
+}
+
+export default BotSubCommandGroup;
