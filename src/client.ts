3d7b14f403cbefdb23d3af26aaf0b6621cf57b65
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-09-12 18:19:36 +0530

    Send welcome message when guild member joins

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,8 @@ import { Client, Intents } from 'discord.js';
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
   ],
 });
 
