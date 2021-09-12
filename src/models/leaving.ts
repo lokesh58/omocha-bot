d99803bb3509bb968e5ac759512b961d8083e6c6
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-09-12 19:01:47 +0530

    Schema for leaving message

---
+++ src/models/leaving.ts
@@ -0,0 +1,14 @@
+import { model, Schema } from 'mongoose';
+import { reqString } from './constants';
+
+export interface Leaving {
+  _id: string,
+  message: string,
+  channelId: string,
+}
+
+export const leavingModel = model<Leaving>('leaving', new Schema<Leaving>({
+  _id: reqString, // guild id
+  message: reqString,
+  channelId: reqString,
+}));
