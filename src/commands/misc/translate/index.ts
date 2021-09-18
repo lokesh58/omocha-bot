c15d1730649316b94583ba6d7f61eb5c6908da9a
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-09-18 21:42:38 +0530

    Added translate command
    1. Added package @iamtraction/google-translate to interact with google translate API.
    2. Translate command
       - Input
           - text: The text to translate
           - from: [optional] The text language
           - to: [optional] The language in which the text should be translated
       - Output
           - original text: The autocorrected original text along with language
           - translated text: The translated text along with language

---
+++ src/commands/misc/translate/index.ts
@@ -0,0 +1,79 @@
+import BotCommand from '../../bot-command';
+import translate from '@iamtraction/google-translate';
+import { MessageEmbed } from 'discord.js';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const languages: { [key: string]: string } = require('@iamtraction/google-translate').languages;
+
+const getISOCode = (language: string) => {
+  if (!language) {
+    return null;
+  }
+  language = language.toLowerCase();
+  if (language in languages) {
+    return language;
+  }
+
+  const keys = Object.keys(languages).filter((key) => {
+    if (typeof languages[key] !== 'string') {
+      return null;
+    }
+
+    return languages[key].toLowerCase() === language;
+  });
+
+  return keys[0] || null;
+};
+
+export default {
+  data: {
+    name: 'translate',
+    description: 'Translate a given text using Google Translate.',
+    options: [
+      {
+        name: 'text',
+        description: 'The text to translate.',
+        type: 'STRING',
+        required: true,
+      },
+      {
+        name: 'from',
+        description: 'The text language (Default: auto).',
+        type: 'STRING',
+        required: false,
+      },
+      {
+        name: 'to',
+        description: 'The language in which the text should be translated (Default: en).',
+        type: 'STRING',
+        required: false,
+      },
+    ]
+  },
+  handler: async (interaction) => {
+    const { options } = interaction;
+    const text = options.getString('text');
+    let from = getISOCode(options.getString('from') ?? 'auto');
+    let to = getISOCode(options.getString('to') ?? 'en');
+    if (!text) {
+      throw new Error('text is null!');
+    }
+    if (!from) {
+      from = 'auto';
+    }
+    if (!to) {
+      to = 'en';
+    }
+    await interaction.deferReply();
+    const result = await translate(text, {
+      from,
+      to,
+    });
+    const embed = new MessageEmbed()
+      .setTitle('Translation Results')
+      .addField(`Original Text - ${languages[result.from.language.iso]}`, result.from.text.value || text)
+      .addField(`Translated Text - ${languages[to]}`, result.text);
+    await interaction.editReply({
+      embeds: [embed],
+    });
+  },
+} as BotCommand;
