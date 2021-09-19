ed4bfde86b08d38a71441f0418d1a9631aa56369
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-09-19 17:23:29 +0530

    message is sent when bot joins a server

--- src/events/index.ts
+++ src/events/index.ts
@@ -5,6 +5,7 @@ const events: {[name: string]: BotEvent} = {
   ready: require('./ready').default,
   welcomeMessage: require('./welcome-message').default,
   leavingMessage: require('./leaving-message').default,
+  guildJoinMessage: require('./guild-join-message').default,
 };
 
 export default events;
