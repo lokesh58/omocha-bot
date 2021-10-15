51a15d87946a375f67ede94ac98ab30a317f3dde
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-10-15 15:18:46 +0530

    Make echo guild only command

--- src/commands/misc/echo/index.ts
+++ src/commands/misc/echo/index.ts
@@ -1,5 +1,6 @@
 import { GuildMember, MessageEmbed, TextBasedChannels } from 'discord.js';
 import BotCommand from '../../bot-command';
+import { guildOnlyError } from '../../command-utils/constants';
 
 export default {
   data: {
@@ -20,25 +21,29 @@ export default {
       },
       {
         name: 'reply_to_message_id',
-        description: 'ID of a message to make sent message a reply to that message.',
+        description: 'ID of a message to make echoed message a reply to that message.',
         type: 'STRING',
         required: false,
       }
     ],
   },
   handler: async (interaction) => {
+    if (!interaction.inGuild()) {
+      await interaction.reply(guildOnlyError);
+      return;
+    }
     const { options, channel: iChannel, member } = interaction;
     const message = options.getString('message', true);
-    const channel = options.getChannel('channel') || iChannel as TextBasedChannels;
+    const channel = options.getChannel('channel') || iChannel;
     const replyMsgId = options.getString('reply_to_message_id') || '';
-    if (channel.type !== 'GUILD_TEXT' && channel.type !== 'DM') {
+    if (!channel || channel.type !== 'GUILD_TEXT') {
       await interaction.reply({
         content: '`channel` must be a text channel',
         ephemeral: true,
       });
       return;
     }
-    if (channel.type === 'GUILD_TEXT' && !(member as GuildMember).permissionsIn(channel).has('SEND_MESSAGES')) {
+    if (!(member as GuildMember).permissionsIn(channel).has('SEND_MESSAGES')) {
       await interaction.reply({
         content: 'You do not have permissions to send messages in the channel',
         ephemeral: true,
