abef1be65f3f822dbdbc7f6f7e89ecdfe800ac05
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-10-17 15:03:43 +0530

    Use the new bot class

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import client from './client';
-import { startEvents } from './utils';
+import { Bot } from './bot';
 
-startEvents(client);
+client.on('ready', () => {
+  new Bot({
+    client,
+    commandsDir: './commands',
+    eventsDir: './events',
+    mongoDbURI: process.env.MONGODB_URI,
+    testGuilds: process.env.TEST_GUILD_ID,
+    botOwners: process.env.OWNER_ID,
+  });
+});
+
 client.login(process.env.TOKEN);
