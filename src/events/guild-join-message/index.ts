78b68a54ea6a9cc596cecf2fc0de54589e3302cc
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-10-23 12:58:37 +0530

    Update events to use the new type

--- src/events/guild-join-message/index.ts
+++ src/events/guild-join-message/index.ts
@@ -1,23 +1,24 @@
 import { MessageEmbed } from 'discord.js';
+import { BotEvent } from '../../bot';
 import { onError } from '../../utils';
-import BotEvent from '../bot-event';
 
-const event: BotEvent = (client) => {
-  client.on('guildCreate', async (guild) => {
-    try{
-      const { systemChannel } = guild;
-      await systemChannel?.send({
-        embeds: [
-          new MessageEmbed()
-            .setTitle(`Thank You for Inviting Me to ${guild.name}`)
-            .setDescription('Use `/` to see all my commands.')
-            .setThumbnail(client.user?.avatarURL({ dynamic: true }) || ''),
-        ],
-      });
-    } catch (err) {
-      onError(err);
-    }
-  });
-};
-
-export default event;
+export default {
+  name: 'send-guild-join-message',
+  starter: (client) => {
+    client.on('guildCreate', async (guild) => {
+      try{
+        const { systemChannel } = guild;
+        await systemChannel?.send({
+          embeds: [
+            new MessageEmbed()
+              .setTitle(`Thank You for Inviting Me to ${guild.name}`)
+              .setDescription('Use `/` to see all my commands.')
+              .setThumbnail(client.user?.avatarURL({ dynamic: true }) || ''),
+          ],
+        });
+      } catch (err) {
+        onError(err);
+      }
+    });
+  },
+} as BotEvent;
--- src/events/leaving-message/index.ts
+++ src/events/leaving-message/index.ts
@@ -1,33 +1,35 @@
+import { BotEvent } from '../../bot';
 import { leavingModel } from '../../models/leaving';
 import { onError } from '../../utils';
-import BotEvent from '../bot-event';
 
-const event: BotEvent = (client) => {
-  client.on('guildMemberRemove', async (member) => {
-    try {
-      const { guild } = member;
-      const leavingDetails = await leavingModel.findById(guild.id);
-      if (!leavingDetails) {
-        return;
+export default {
+  name: 'send-leaving-message',
+  starter: (client) => {
+    client.on('guildMemberRemove', async (member) => {
+      try {
+        const { guild } = member;
+        const leavingDetails = await leavingModel.findById(guild.id);
+        if (!leavingDetails) {
+          console.log(`Leaving Details not found for guild ${guild.id}`);
+          return;
+        }
+        const { channelId, message } = leavingDetails;
+        let channel = guild.channels.cache.get(channelId) ?? null;
+        if (!channel) {
+          channel = await guild.channels.fetch(channelId);
+        }
+        if (!channel) {
+          throw new Error(`Unable to get the leaving message channel for guild with id ${guild.id}!`);
+        }
+        if (!channel.isText()) {
+          throw new Error(`Leaving channel is not text channel for guild with id ${guild.id}!`);
+        }
+        await channel.send({
+          content: message.replace(/<@>/g, `${member} (${member.user?.tag || member.displayName})`),
+        });
+      } catch (err) {
+        onError(err);
       }
-      const { channelId, message } = leavingDetails;
-      let channel = guild.channels.cache.get(channelId) ?? null;
-      if (!channel) {
-        channel = await guild.channels.fetch(channelId);
-      }
-      if (!channel) {
-        throw new Error(`Unable to get the leaving message channel for guild with id ${guild.id}!`);
-      }
-      if (!channel.isText()) {
-        throw new Error(`Leaving channel is not text channel for guild with id ${guild.id}!`);
-      }
-      await channel.send({
-        content: message.replace(/<@>/g, `<@${member.id}> (${member.user?.tag || member.displayName})`),
-      });
-    } catch (err) {
-      onError(err);
-    }
-  });
-};
-
-export default event;
+    });
+  },
+} as BotEvent;
