3848ea28b1f59843cf0f99febb12af44ecf06a3c
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-10-30 15:47:00 +0530

    Add autocomplete support for languages

--- src/commands/misc/translate.ts
+++ src/commands/misc/translate.ts
@@ -2,7 +2,9 @@ import translate from '@iamtraction/google-translate';
 import { MessageEmbed } from 'discord.js';
 import { BotCommand } from '../../bot';
 // eslint-disable-next-line @typescript-eslint/no-var-requires, prefer-destructuring
-const languages: { [key: string]: string } = require('@iamtraction/google-translate').languages;
+const languages: { [key: string]: string } = Object.entries(require('@iamtraction/google-translate').languages)
+  .filter(([, v]) => typeof v === 'string')
+  .reduce((a, [k, v]) => ({ ...a, [k]: v }), {});
 
 const getISOCode = (language: string | null) => {
   if (!language) {
@@ -36,14 +38,46 @@ export default {
       description: 'The text language (Default: auto).',
       type: 'STRING',
       required: false,
+      autocomplete: true,
     },
     {
       name: 'to',
       description: 'The language in which the text should be translated (Default: en).',
       type: 'STRING',
       required: false,
+      autocomplete: true,
     },
   ],
+  autocompleteHandler: async (interaction) => {
+    const { options } = interaction;
+    const value = options.getFocused();
+    if (typeof value === 'string') {
+      const getMatchScore = (k: string, v: string) => {
+        if (k.startsWith(value) || v.startsWith(value)) {
+          return 6;
+        } else if (v.toLowerCase().startsWith(value)) {
+          return 5;
+        } else if (k.startsWith(value.toLowerCase()) || v.toLowerCase().startsWith(value.toLowerCase())) {
+          return 4;
+        } else if (k.includes(value) || v.includes(value)) {
+          return 3;
+        } else if (v.toLowerCase().includes(value)) {
+          return 2;
+        } else if (k.includes(value.toLowerCase()) || v.toLowerCase().includes(value.toLowerCase())) {
+          return 1;
+        }
+        return 0;
+      };
+      const matchScores: {[key: string]: number} = Object.entries(languages).reduce((s, [k, v]) => ({ ...s, [k]: getMatchScore(k, v) }), {});
+      const languageOptions = Object.keys(languages)
+        .filter(k => matchScores[k] > 0)
+        .sort((a, b) => matchScores[a] - matchScores[b])
+        .slice(0, 25);
+      await interaction.respond(languageOptions.map(k => ({ name: languages[k], value: k })));
+    } else {
+      await interaction.respond([]);
+    }
+  },
   handler: async (interaction) => {
     const { options } = interaction;
     const text = options.getString('text', true);
